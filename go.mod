29057751cc58b58a1edbd501d684bfc0aca0113d
Jimmy Chuang <ogre0403@gmail.com>
2021-11-24 09:31:43 +0800

    add code-generator dependency

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	k8s.io/apimachinery v0.19.15
 	k8s.io/client-go v0.19.15
+	k8s.io/code-generator v0.19.15
 )
--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,18 +18,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   gitlab.com/nchc-ai/course-crd/pkg/client gitlab.com/nchc-ai/course-crd/pkg/apis \
   coursecontroller:v1alpha1 \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
-#  --output-base "$(dirname ${BASH_SOURCE})/../../.."
+#  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 
 # To use your own boilerplate text use:
